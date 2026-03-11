9003eb7024a667bd82e18a6c1b50dbd65301126a
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-03-11 20:56:01 +0100

    Create hashPassword

--- Backend/src/utils/hashPassword.ts
+++ Backend/src/utils/hashPassword.ts
@@ -1,2 +1,10 @@
 // hash password - HJÄLPER FUNKTION
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 12);
+};
+
+export const comparePassword = async (password: string, hash: string) => {
+    return await bcrypt.compare(password, hash);
+};
