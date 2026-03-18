5809d55779ccfc9f8bbdb4b5bbec56d2a5f2fd63
HannaMarinadotter <hannamarinadotter@outlook.com>
2026-03-18 15:51:49 +0100

    feat: add Order interface and createOrder function

--- Backend/src/models/mysql/order.model.ts
+++ Backend/src/models/mysql/order.model.ts
@@ -1,1 +1,24 @@
 // order model mysql
+
+import pool from "../../config/mysql";
+
+export interface Order {
+  order_id?: number;
+  user_id: number;
+  created_at?: Date;
+  total_price?: number;
+}
+
+// create order
+export const createOrder = async (order: Order): Promise<number> => {
+  const { user_id, total_price } = order;
+
+  const [result]: any = await pool.execute(
+    `INSERT INTO Orders (user_id, total_price) VALUES (?, ?)`,
+    [user_id, total_price || 0]
+  );
+
+  return result.insertId;
+};
+
+
