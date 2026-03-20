9f0567635ac9e5ae4fdb2b3df686ee67aebfd272
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-03-20 10:33:24 +0100

    Fix link in members.js

--- Frontend/assets/js/member.js
+++ Frontend/assets/js/member.js
@@ -1,55 +1,52 @@
-const token = localStorage.getItem('token')
-const userId = localStorage.getItem('userId')
+const token = localStorage.getItem("token");
+// const userId = localStorage.getItem("userId");
 
 if (!token || !userId) {
-    alert('Du måste logga in först')
-    window.location.href = '../pages/login.html'
+  alert("Du måste logga in först");
+  window.location.href = "../pages/login.html";
 }
 
-const memberName = document.getElementById('member-name')
-const logoutBtn = document.getElementById('logout-btn')
+const memberName = document.getElementById("member-name");
+const logoutBtn = document.getElementById("logout-btn");
 
 async function loadMemberPage() {
-    try {
-        const response = await fetch(
-            `http://localhost:3000/api/users/${userId}`,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`
-                }
-            }
-        )
+  try {
+    const response = await fetch(`http://localhost:3000/api/auth/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-        const data = await response.json()
+    const data = await response.json();
 
-        if (!response.ok) {
-            throw new Error(data.message || 'Kunde inte hämta användardata')
-        }
-
-        memberName.textContent = data.data.name
-    } catch (error) {
-        console.error(error)
-        alert('Sessionen är ogiltig, logga in igen')
-
-        localStorage.removeItem('token')
-        localStorage.removeItem('userId')
-        localStorage.removeItem('name')
-
-        window.location.href = '../pages/login.html'
+    if (!response.ok) {
+      throw new Error(data.message || "Kunde inte hämta användardata");
     }
+
+    memberName.textContent = data.data.name;
+  } catch (error) {
+    console.error(error);
+    alert("Sessionen är ogiltig, logga in igen");
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("name");
+
+    window.location.href = "../pages/login.html";
+  }
 }
 
 function logout() {
-    localStorage.removeItem('token')
-    localStorage.removeItem('userId')
-    localStorage.removeItem('name')
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("name");
 
-    alert('Du har loggats ut.')
-    window.location.href = '../pages/login.html'
+  alert("Du har loggats ut.");
+  window.location.href = "../pages/login.html";
 }
 
-logoutBtn.addEventListener('click', logout)
+logoutBtn.addEventListener("click", logout);
 
-loadMemberPage()
+loadMemberPage();
